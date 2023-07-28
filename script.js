99d9927efd2146df3de03052684e213724e8f24c
jet11301 <phuochung11301@gmail.com>
2023-07-28 12:51:34 +0700

    init project

---
+++ script.js
@@ -0,0 +1,14 @@
+//
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterableCards = document.querySelectorAll(".fillerable_cards .card");
+const filterCards = e =>{
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    filterableCards.forEach(card=>{
+    card.classList.add("hide");
+    if( card.dataset.name === e.target.dataset.name || e.target.dataset.name ==="all"){
+       card.classList.remove("hide");
+    }
+    });
+}
+filterButtons.forEach(button =>button.addEventListener("click",filterCards));
